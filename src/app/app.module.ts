e8ef7d714e265f5c58967cd0bcc37e53a4e0b23a
solankepl <solankepl@gmail.com>
2017-06-02 15:04:15 +0530

    adding and delete data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,14 @@ import { FormComponent } from './form/form.component';
 import { HomeComponent } from './home/home.component';
 import { Subtab1Component } from './home/subtab/subtab1/subtab1.component';
 import { Subtab2Component } from './home/subtab/subtab2/subtab2.component';
+import { Subtab3Component } from './home/subtab/subtab3/subtab3.component';
 
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
 
 import { StudentdataService } from './services/studentdata.service';
 import { StudentlistComponent } from './commancomponent/studentlist/studentlist.component';
-//import { SortinglistPipe } from './pipe/sortinglist.pipe';
+import { SortinglistPipe } from './pipe/sortinglist.pipe';
 
 
 
@@ -28,7 +29,8 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'subtab1', pathMatch: 'full' },
       { path: 'subtab1', component: Subtab1Component},
-      { path: 'subtab2', component: Subtab2Component }
+      { path: 'subtab2', component: Subtab2Component },
+      { path: 'subtab3', component: Subtab3Component }
     ]     
    },
   { path: 'about', component: AboutComponent },
@@ -36,11 +38,10 @@ const routes: Routes = [
   { path: '**', component: PageNotFoundComponentComponent }    
 ];
 
-
-
 @NgModule({
   declarations: [
     AppComponent,
+    SortinglistPipe,  
     HeadertopComponent,
     FooterBottomComponent,
     AboutComponent,
@@ -49,6 +50,7 @@ const routes: Routes = [
     PageNotFoundComponentComponent,
     Subtab1Component,
     Subtab2Component,
+      Subtab3Component,
     StudentlistComponent
   ],
   imports: [
@@ -58,6 +60,7 @@ const routes: Routes = [
     RouterModule.forRoot(routes) // <-- installs Router routes, components and services-->
   ],
   providers: [StudentdataService], 
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+    
 })
 export class AppModule { }
---
+++ src/app/home/subtab/subtab3/subtab3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subtab3',
+  templateUrl: './subtab3.component.html',
+  styleUrls: ['./subtab3.component.css']
+})
+export class Subtab3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
